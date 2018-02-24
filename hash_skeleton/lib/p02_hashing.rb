a55188e04bd9fbddf4b71c90c9bba3ad0ffda44a
Michael Valeriani <michaelvaleriani@gmail.com>
2018-02-23 16:25:09 -0800

    Implement Array hash, String hash, and Hash hash

--- hash_skeleton/lib/p02_hashing.rb
+++ hash_skeleton/lib/p02_hashing.rb
@@ -4,11 +4,31 @@ end
 
 class Array
   def hash
+    ords = self.map { |el| el.to_s.ord }
+
+    xor_res = 0
+    ords[0...-1].each_index do |idx|
+      curr_ord = ords[idx]
+      next_ord = ords[idx+1]
+
+      xor_res += curr_ord ^ next_ord
+    end
+    xor_res
   end
 end
 
 class String
   def hash
+    ords = self.chars.map{|chr| chr.ord }
+
+    xor_res = 0
+    ords[0...-1].each_index do |idx|
+      curr_ord = ords[idx]
+      next_ord = ords[idx+1]
+
+      xor_res += curr_ord ^ next_ord
+    end
+    xor_res
   end
 end
 
@@ -16,6 +36,15 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    ords = self.map {|k,v| k.to_s.ord + v.to_s.ord }
+
+    xor_res = 0
+    ords[0...-1].each_index do |idx|
+      curr_ord = ords[idx]
+      next_ord = ords[idx+1]
+
+      xor_res += curr_ord ^ next_ord
+    end
+    xor_res
   end
 end
